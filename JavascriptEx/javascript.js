cd9a00538383f6cf2bea913baaba0fb955219250
doquocsang <quocsang316@gmail.com>
2024-11-03 22:52:37 +0700

    Xong phần 2.2 JS (còn 3 bài)

--- JavascriptEx/javascript.js
+++ JavascriptEx/javascript.js
@@ -1,4 +1,5 @@
 
+// >>> PART 1 <<< //
 // Ex 1 ===================================================
 // Design a function which returns a fibonacci sequence value
 // ========================================================
@@ -86,4 +87,94 @@ function CountVowel(str) {
 // console.log(`"${str}" has "${CountVowel(str)}" vowel(s)`,);
 
 
+// ========================================================
 
+
+// >>> PART 2 <<< //
+// Ex 1 ===================================================
+// Write a function to convert a string to title case
+// ========================================================
+// const str = prompt('Enter string: ');
+
+function ConvertToTitleCase(str) {
+    var arr = str.toLowerCase().trim().split(" ");
+
+    // arr.forEach((word, index) => {
+    //     arr[index] = word.charAt(0).toUpperCase() + word.slice(1);
+    // });
+
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+
+    return arr.join(" ");
+}
+
+// console.log(`"${str}" ---> "${ConvertToTitleCase(str)}"`,);
+
+// Ex 2 ===================================================
+// Write a function which can convert the time input given in 12 hours format to 24 hours format
+// ========================================================
+// const str = prompt('Enter time (hh:mm AM/PM): ');
+function FormatNumber(arr) {
+    for(let i = 0; i < arr.length; i++){
+        if(!arr[i].match(/0\d/g) && parseInt(arr[i]) < 10) {
+            arr[i] = '0' + arr[i];
+        }
+    }
+}
+
+function CheckType(type, hour) {
+    let result = hour;
+    switch(type) {
+        case "AM":
+            if(hour === "12") result = "00";
+            break;
+        case "PM":
+            if(hour !== "12") result = parseInt(hour) + 12 + "";
+            break;
+        default:
+          console.log("Wrong time format!!!");
+    }
+    return result;
+}
+
+function ConvertTime(str) {
+    let type = str.match(/[a-zA-z]+$/).toString().toUpperCase();
+    let timeArr = str.match(/\d+:\d+/g).toString().split(":");
+
+    // Convert hour by type
+    timeArr[0] = CheckType(type, timeArr[0]);
+
+    // Format num before view
+    FormatNumber(timeArr);
+
+    return timeArr.join(":");
+}
+
+// console.log(`"${str}" ---> "${ConvertTime(str)}"`,);
+
+// Ex 3 ===================================================
+// Map data to frontend format. The main element is location key and we need to map all data to it. We will have 5 objects at the end.
+// ========================================================
+
+
+
+// Ex 4 ===================================================
+// Write a function to Replace parameters in url
+// ========================================================
+const initialUrl = "/posts/:postId/comments/:commentId";
+
+console.log(initialUrl.match(/:\w+/g))
+
+console.log(initialUrl.replace(/:\w+/g, '1'))
+
+function replaceParamsInUrl(initialUrl, args){
+    
+}
+// const resultUrl = replaceParamsInUrl(initialUrl, [
+//   { from: "postId", to: "1" },
+//   { from: "commentId", to: "3" },
+// ]);
+
+// console.log(resultUrl);
